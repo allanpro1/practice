8a97aaf6daa402c13a37d642a75d71a64e1e7c44
allanpro1 <allankiggund1997@gmail.com>
2022-02-22 17:33:19 +0300

    added delete items, display items in both table and main shop and also  logic to avoid user from access main shop if he is not signed in

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ firebase.initializeApp(firebaseConfig);
 // ......................firebase authentication (signup)...............................
 
 auth = firebase.auth();
+db = firebase.firestore();
 // function to manage signup when we click sign up button
 
 function sign_up(){
@@ -116,10 +117,151 @@ function forgot_pass(){
     auth.sendPasswordResetEmail(email);
     alert("check your email");
 
-    window.location.href="login.html";
+    window.location.href="index.html";
 
 }
 
+
+
+
+
+
+
+//....................................log out fmction ..................................
+
+
+function logout(){
+// import { getAuth, signOut } from "firebase/auth";
+
+auth = firebase.auth(); 
+auth.signOut().then(() => {
+
+
+  // Sign-out successful.
+  window.location.href="index.html";
+}).catch((error) => {
+    alert("login failed"+ errorMessage);
+  // An error happened.
+});
+
+}
+
+
+
+//............................state observer to always prevrnt user from accessing other pages ifnot signed in............................
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+    } else {
+      if (window.location.pathname=="/mainshop.html" ){
+
+        window.location.href="index.html";
+      }
+      // No user is signed in.
+    }
+  });
+
+
+
+  // for sending data to the bd ...................... in the damin hml
+
+
+  function upload_to_db(){
+    // Add a new document with a generated id.
+
+
+    // remember to initialize db up in the initialize function 
+ db.collection("Products").add({
+
+  // to be used when calling form the db 
+  item_name: document.getElementById('iten').value,
+  item_price: document.getElementById('price').value,
+  maker:document.getElementById('maker').value
+
+
+})
+.then((docRef) => {
+  console.log("Document written with ID: ", docRef.id);
+  alert('success');
+})
+.catch((error) => {
+  alert('fail'+errorMessage);
+
+  console.error("Error adding document: ", error);
+});
+  }
+
+
+  // read them into table and then main shop
+  // for table...................................
+
+  function read()
+  {
+
+
+    db.collection("Products")
+
+    .onSnapshot((querySnapshot) => {
+
+        querySnapshot.forEach((doc) => {
+ product = doc.data();
+          // regrence to your table
+         table_ref = document.getElementById('admin_table').getElementsByTagName('tbody')[0];
+// inser row after the last index always
+         newRow = table_ref.insertRow(-1);
+
+        //  INSTRUCT THE JS ON HOW TO FILL IN THE ROW
+         //  key names that we used wheninputing in the db iin upload function
+         newRow.innerHTML = `<tr> 
+       
+         <td> `+ product["item_name"] + `</td>
+         <td>`+ product["item_price"] +`</td>
+          <td>`+product["maker"] +`</td>
+          <td></td>
+         <td>
+          
+           <button class="btn" onClick="delete_itens('`+ doc.id +`')"><img src="trash.png">  </button>
+         </td>
+        </tr>`
+
+
+        });
+    });
+  }
+
+
+  // for the main shop 
+
+  function   read_main(){
+    db.collection("Products")
+    .onSnapshot((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          console.log("yeh");
+          product = doc.data();
+          div_ref = document.getElementById('items');
+
+          div_ref.innerHTML+= `<div lass="item_card" ><img src="" alt="to hold the image">
+          <h3>`+ product["item_name"] + `</h3>
+          <h5>`+ product["item_price"] +`</h5>
+          <h6>`+product["maker"] +`</h6></div>`;
+
+        });
+    });
+
+  }
+
+
+  // delete data in the table (admin side)
+
+  function delete_itens(id){
+    db.collection("Products").doc(id).delete().then(() => {
+      console.log("Document successfully deleted!");
+  }).catch((error) => {
+      console.error("Error removing document: ", error);
+  });
+
+  }
 
 
 
