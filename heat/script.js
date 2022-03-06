bf7ecb68fff6067ffa347978f6453897d4ba377f
allanpro1 <allankiggund1997@gmail.com>
2022-03-06 21:42:46 +0300

    add to cart done

--- heat/script.js
+++ heat/script.js
@@ -1,20 +1,20 @@
-  // Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
 //   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCAaEZLL2XTmRYsyfHaCGzeDkM8n3q2Zg4",
-    authDomain: "writereadtrial.firebaseapp.com",
-    databaseURL: "https://writereadtrial-default-rtdb.firebaseio.com",
-    projectId: "writereadtrial",
-    storageBucket: "writereadtrial.appspot.com",
-    messagingSenderId: "892636068322",
-    appId: "1:892636068322:web:2141cd17c4789e3845a1bb"
-  };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCAaEZLL2XTmRYsyfHaCGzeDkM8n3q2Zg4",
+  authDomain: "writereadtrial.firebaseapp.com",
+  databaseURL: "https://writereadtrial-default-rtdb.firebaseio.com",
+  projectId: "writereadtrial",
+  storageBucket: "writereadtrial.appspot.com",
+  messagingSenderId: "892636068322",
+  appId: "1:892636068322:web:2141cd17c4789e3845a1bb"
+};
 
-  // Initialize Firebase
+// Initialize Firebase
 //   const app = initializeApp(firebaseConfig);
 firebase.initializeApp(firebaseConfig);
 
@@ -25,99 +25,99 @@ auth = firebase.auth();
 db = firebase.firestore();
 // function to manage signup when we click sign up button
 
-function sign_up(){
-    // initializing variables for the different inputs we have
-    email = document.getElementById('user_email').value;
-    
-    password = document.getElementById('user_pass').value;
+function sign_up() {
+  // initializing variables for the different inputs we have
+  email = document.getElementById('user_email').value;
 
-    userName = document.getElementById('user_name').value;
+  password = document.getElementById('user_pass').value;
 
-    // statement to alert user that he has not put an email
+  userName = document.getElementById('user_name').value;
 
-    if(email == ""){
-        alert("mail cant be empty!");
-    }
+  // statement to alert user that he has not put an email
 
-    
-    firebase.auth().createUserWithEmailAndPassword(email, password)
+  if (email == "") {
+    alert("mail cant be empty!");
+  }
+
+
+  firebase.auth().createUserWithEmailAndPassword(email, password)
     .then((userCredential) => {
       // Signed in 
       alert("sucess!");
       var user = userCredential.user;
 
-    //   code to save userName in Firebase aUth
+      //   code to save userName in Firebase aUth
       user.updateProfile({
-          displayName: userName
+        displayName: userName
       }).then(() => {
-          //   ......................push user to the main or shop part when he has signed up successfully............................
-        window.location.href="mainshop.html";
+        //   ......................push user to the main or shop part when he has signed up successfully............................
+        window.location.href = "mainshop.html";
 
 
       })
-    
+
       // ...
     })
     .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // incase it fails
-        alert("creating failed"+ errorMessage);
-     
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // incase it fails
+      alert("creating failed" + errorMessage);
+
       // ..
     });
 
 
-} 
+}
 
 
 
 // the code for log in
 
-function login_in(){
-    email = document.getElementById('user_mail').value;
-    
-    password = document.getElementById('user_password').value;
+function login_in() {
+  email = document.getElementById('user_mail').value;
 
-    // statement to alert user that he has not put an email
+  password = document.getElementById('user_password').value;
 
-    if(email == ""){
-        alert("mail cant be empty!");
-    }
+  // statement to alert user that he has not put an email
 
-    
-    firebase.auth().signInWithEmailAndPassword(email, password)
+  if (email == "") {
+    alert("mail cant be empty!");
+  }
+
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
     .then((userCredential) => {
       // Signed in 
       alert("sucess!");
       var user = userCredential.user;
-    //   ......................push user to the main or shop part when he has signed up successfully............................
-        window.location.href="mainshop.html";
+      //   ......................push user to the main or shop part when he has signed up successfully............................
+      window.location.href = "mainshop.html";
 
       // ...
     })
     .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // incase it fails
-        alert("login failed"+ errorMessage);
-     
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // incase it fails
+      alert("login failed" + errorMessage);
+
       // ..
     });
 
 
-} 
+}
 
 
 // forgot password implementation
 
-function forgot_pass(){
-    email=document.getElementById('email_to_reset').value;
+function forgot_pass() {
+  email = document.getElementById('email_to_reset').value;
 
-    auth.sendPasswordResetEmail(email);
-    alert("check your email");
+  auth.sendPasswordResetEmail(email);
+  alert("check your email");
 
-    window.location.href="index.html";
+  window.location.href = "index.html";
 
 }
 
@@ -130,19 +130,19 @@ function forgot_pass(){
 //....................................log out fmction ..................................
 
 
-function logout(){
-// import { getAuth, signOut } from "firebase/auth";
+function logout() {
+  // import { getAuth, signOut } from "firebase/auth";
 
-auth = firebase.auth(); 
-auth.signOut().then(() => {
+  auth = firebase.auth();
+  auth.signOut().then(() => {
 
 
-  // Sign-out successful.
-  window.location.href="index.html";
-}).catch((error) => {
-    alert("login failed"+ errorMessage);
-  // An error happened.
-});
+    // Sign-out successful.
+    window.location.href = "index.html";
+  }).catch((error) => {
+    alert("login failed" + errorMessage);
+    // An error happened.
+  });
 
 }
 
@@ -150,118 +150,117 @@ auth.signOut().then(() => {
 
 //............................state observer to always prevrnt user from accessing other pages ifnot signed in............................
 
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-    } else {
-      if (window.location.pathname=="/mainshop.html" ){
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in.
+  } else {
+    if (window.location.pathname == "/mainshop.html") {
 
-        window.location.href="index.html";
-      }
-      // No user is signed in.
+      window.location.href = "index.html";
     }
-  });
-
-
-
-  // for sending data to the bd ...................... in the damin hml
-
-
-  function upload_to_db(){
-    // Add a new document with a generated id.
-
-
-    // remember to initialize db up in the initialize function 
- db.collection("Products").add({
-
-  // to be used when calling form the db 
-  item_name: document.getElementById('iten').value,
-  item_price: document.getElementById('price').value,
-  maker:document.getElementById('maker').value,
-  gender:document.getElementById('radio_button').value,
-  car_type:document.getElementById('cars').value,
-  paymentMethod:document.getElementById('cash').checked,
-  paymentMethod2:document.getElementById('check').checked,
-
-
-
-
-
-
-})
-.then((docRef) => {
-  console.log("Document written with ID: ", docRef.id);
-  alert('success');
-})
-.catch((error) => {
-  alert('fail'+errorMessage);
-
-  console.error("Error adding document: ", error);
-});
+    // No user is signed in.
   }
+});
 
 
-  // read them into table and then main shop
-  // for table...................................
 
-  function read()
-  {
+// for sending data to the bd ...................... in the damin hml
 
 
-    db.collection("Products")
+function upload_to_db() {
+  // Add a new document with a generated id.
+
+
+  // remember to initialize db up in the initialize function 
+  db.collection("Products").add({
+
+    // to be used when calling form the db 
+    item_name: document.getElementById('iten').value,
+    item_price: document.getElementById('price').value,
+    maker: document.getElementById('maker').value,
+    gender: document.getElementById('radio_button').value,
+    car_type: document.getElementById('cars').value,
+    paymentMethod: document.getElementById('cash').checked,
+    paymentMethod2: document.getElementById('check').checked,
+
+
+
+
+
+
+  })
+    .then((docRef) => {
+      console.log("Document written with ID: ", docRef.id);
+      alert('success');
+    })
+    .catch((error) => {
+      alert('fail' + errorMessage);
+
+      console.error("Error adding document: ", error);
+    });
+}
+
+
+// read them into table and then main shop
+// for table...................................
+
+function read() {
+
+
+  db.collection("Products")
 
     .onSnapshot((querySnapshot) => {
 
-        querySnapshot.forEach((doc) => {
- product = doc.data();
-          // regrence to your table
-         table_ref = document.getElementById('admin_table').getElementsByTagName('tbody')[0];
-// inser row after the last index always
-         newRow = table_ref.insertRow(-1);
+      querySnapshot.forEach((doc) => {
+        product = doc.data();
+        // regrence to your table
+        table_ref = document.getElementById('admin_table').getElementsByTagName('tbody')[0];
+        // inser row after the last index always
+        newRow = table_ref.insertRow(-1);
 
         //  INSTRUCT THE JS ON HOW TO FILL IN THE ROW
-         //  key names that we used wheninputing in the db iin upload function
-         newRow.innerHTML = `<tr> 
+        //  key names that we used wheninputing in the db iin upload function
+        newRow.innerHTML = `<tr> 
        
          <td> `+ product["item_name"] + `</td>
-         <td>`+ product["item_price"] +`</td>
-          <td>`+product["maker"] +`</td>
+         <td>`+ product["item_price"] + `</td>
+          <td>`+ product["maker"] + `</td>
           
 
 
           <td></td>
           
           <td> `+ product["gender"] + `</td>
-          <td>`+ product["car_type"] +`</td>
-           <td>`+product["paymentMethod"] +`</td>
-           <td>`+product["paymentMethod2"] +`</td>
+          <td>`+ product["car_type"] + `</td>
+           <td>`+ product["paymentMethod"] + `</td>
+           <td>`+ product["paymentMethod2"] + `</td>
 
          <td>
           
-           <button class="btn" onClick="delete_itens('`+ doc.id +`')"><img src="trash.png">  </button>
+           <button class="btn" onClick="delete_itens('`+ doc.id + `')"><img src="trash.png">  </button>
          </td>
         </tr>`
 
 
-        });
+      });
     });
-  }
+}
 
 
-  // for the main shop 
+// for the main shop 
 
-  function   read_main(){
-    db.collection("Products")
+function read_main() {
+  db.collection("Products")
     .onSnapshot((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          console.log("yeh");
-          product = doc.data();
-          div_ref = document.getElementById('items');
+      querySnapshot.forEach((doc) => {
+        console.log("yeh");
+        product = doc.data();
+        div_ref = document.getElementById('items');
 
-          div_ref.innerHTML+= `<div class="item_card" ><img src="" alt="to hold the image">
-          <h3>`+ product["item_name"] + `</h3>
-          <h5>`+ product["item_price"] +`</h5>
-          <h6>`+product["maker"] +`</h6></div>
+        div_ref.innerHTML += `<div class="item_card" ><img src="" alt="to hold the image">
+          <h3 id ="ItemName">`+ product["item_name"] + `</h3>
+          <h5>`+ product["item_price"] + `</h5>
+          <h6>`+ product["maker"] + `</h6></div>
           <select id="mumber" name="number">
       <option value="1">1 </option>
       <option value="2">2</option>
@@ -274,47 +273,73 @@ firebase.auth().onAuthStateChanged(function(user) {
       <option value="9">9</option>
       <option value="10">10</option>
     </select> <br><br>
-    <button type="submit" id="add_to_cart" onclic="addToCart()"> Add to cart</button><br><br><br>
+    <button type="submit" id="add_to_cart" onclick="addToCart('`+ doc.id + `')"> Add to cart</button><br><br><br>
 
-    ` 
-    ;
+    `
+          ;
 
-        });
+      });
     })
 
-  }
+}
 
 
-  // delete data in the table (admin side)
+// delete data in the table (admin side)
 
-  function delete_itens(id){
-    db.collection("Products").doc(id).delete().then(() => {
-      console.log("Document successfully deleted!");
+function delete_itens(id) {
+  db.collection("Products").doc(id).delete().then(() => {
+    console.log("Document successfully deleted!");
   }).catch((error) => {
-      console.error("Error removing document: ", error);
+    console.error("Error removing document: ", error);
   });
 
-  }
+}
 
 
+// add to cart
+// go to line 277 and look at how I made the add to cart button
+function addToCart(product) {
 
-  // to upload pic
+  db.collection("cart").add(
+    // this creates a unique id of the product being added to cart to make it unique
+    {
+      "products_id": product,
+      // to pic the quantity  of things 
+      "qty": document.getElementById('mumber').value,
+      "owner_id": firebase.auth().currentUser.uid,
+
+    }
+
+  ).then(()=>{
+    alert("Item Added to cart😎")  })
+}
 
 
+// now reading the cart things for the user
+async function read_cart()   {
+ user = await firebase.auth().currentUser,
+ uid = user.uid,
 
+  // the where clause makes sure it only reads items collected by that specific user 
+  db.collection("cart").where('owner_id', '==', "uid")
+    .onSnapshot((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
 
+        console.log(doc.data());
+        product = doc.data();
+        // div_ref = document.getElementById('items');
 
-  // validator 
+        // div_ref.innerHTML += `
+        //   <h3>`+ product[""] + `</h3>
+        //   <h5>`+ product["qty"] + `</h5>
+        //   <h6>`+ product["maker"] + `</h6></div>
+        //   <select id="mumber" name="number">`
+        //   ;
 
-  // function validate(){
-  //   var phoneNumber = document.getElementById('phone-number').value;
-  //   var postalCode = document.getElementById('postal-code').value;
-  //   var phoneRGEX = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
-  //   var postalRGEX = "^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$";
-  //   var phoneResult = phoneRGEX.test(phoneNumber);
-  //   var postalResult = postalRGEX.test(postalCode);
-  //   alert("phone:"+phoneResult + ", postal code: "+postalResult);
-  // }
+      });
+    })
+
+}
 
 
 
